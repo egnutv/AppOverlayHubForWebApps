3e6e09d564fde3a1db305c2cb7c50f77d1399c6b
egnutv <101817247+egnutv@users.noreply.github.com>
2023-12-01 00:02:37 +0100

    Optimizing

--- src/scripts/main/main.js
+++ src/scripts/main/main.js
@@ -4,68 +4,6 @@ window.onload = function() {
 
 
 
-//Event if was an object clicked
-document.addEventListener("DOMContentLoaded", function() {
-    ActiveEvent();
-    AutoZIndex();
-
-});
-var stepCounter = 0;
-    
-
-function AutoZIndex() {
-
-    let body = document.querySelector("body");
-
-    let children = body.children;
-
-    for (let i = 0; i < children.length; i++) {
-
-        children[i].style.zIndex = i + 1;
-    }
-}
-
-
-function ActiveEvent() {
-    let buttons = document.querySelectorAll("input[type='button']");
-
-    buttons.forEach(button => {
-        let events = [];
-        button.addEventListener('click', function() {
-            events.push('click');
-            this.id = 'active_clicked';
-            setTimeout(() => {
-                this.id = '';
-            }, 500);
-        })
-        button.addEventListener('mouseover', function() {
-            events.push('mouseover');
-            this.id = 'activate_hovered';
-        })
-        button.addEventListener('mouseout', function() {
-            events = [];
-            this.id = '';
-        })
-    });
-}
-
-
-
-
-// Senden Sie die currentTheme-Variable an alle iFrames, wenn sie geladen sind
-window.addEventListener('DOMContentLoaded', (event) => {
-    postMessageToAllIframes(currentTheme);
-});
-
-//Button Functions
-//Darkmode Changer
-// Zählervariable für die Schritte
-// Definieren Sie die Variablen
-
-
-
-
-
 
 
 
