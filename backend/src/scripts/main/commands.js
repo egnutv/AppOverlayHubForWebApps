8c88f4c73e5656ae61e6cfa1dc71184a347995dc
egnutv <101817247+egnutv@users.noreply.github.com>
2024-01-18 15:18:37 +0100

    written more in commands

--- backend/src/scripts/main/commands.js
+++ backend/src/scripts/main/commands.js
@@ -2,6 +2,11 @@ var command = Null;
 function commands(command, args) {
     
     switch (command) {
-        
+        case system:
+            break;
+        case game:
+            break;
+        default:
+            break;
     }
 }
