9d1e0d537a8d37c4fc5fee3792f399eccc728b39
egnutv <101817247+egnutv@users.noreply.github.com>
2024-01-31 14:07:39 +0100

    Add comands

---
+++ backend/src/scripts/main/command.js
@@ -0,0 +1,30 @@
+function command(command, array){
+    switch (command) {
+        case slideTo:
+                importScripts(array)
+      
+        break;
+    }
+}
+function importScripts(){
+    let scriptImport;
+    scriptImport = new ScriptImport();
+    
+    var script = "testtext";
+    scriptImport.setScript(script);
+}
+class ScriptImportHeandler{
+    setScript(script){
+        var para = document.createElement("p");
+        var node = document.createTextNode(script);
+        para.appendChild(node);
+
+        document.head.appendChild(para);
+    }
+    getScipt(){
+
+    }
+    rmScript(){
+
+    }
+}
