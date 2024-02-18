be3229dbb3f824c58bf93676cc673d4fbdf0943f
egnutv <101817247+egnutv@users.noreply.github.com>
2024-02-18 07:57:54 +0100

    renaming
    scriptHandler.js was renamed to scriptAddHandler.js. Because it is more logical.

--- re/background/src/scripts/main/handler/executeHandler.js
+++ re/background/src/scripts/main/handler/executeHandler.js
@@ -2,7 +2,7 @@ function chainedExecute(aChain) {
     var chain = [aChain];
 
     for (let i = 0; i < chain.length; i++) {
-        this.execute(chain[i]);
+        this.execute(chain[i]); //Hier wird der Wert aus dem Array, der Nummer einer function die es ausführt übergeben
     }
 
 }   
