b63b4944c5806d89f204c4d0404fe48dd8015754
egnutv <101817247+egnutv@users.noreply.github.com>
2024-03-21 15:39:58 +0100

    continuation

---
+++ scripts/controller/storageController/copyPasteRemoveController/newSiteController.js
@@ -0,0 +1,11 @@
+import { GetSetRemoveServerToClientHelper } from "../../../helper/storageHelper/GetSetRemoveServerToClientFileHelper.js";
+
+class SiteController{
+    constructor() {
+        this.value = new GetSetRemoveServerToClientHelper;
+    }
+    async create(){
+        
+    }
+}
+export { SiteController }
--- scripts/helper/storageHelper/GetSetRemoveServerToClientFileHelper.js
+++ scripts/helper/storageHelper/GetSetRemoveServerToClientFileHelper.js
@@ -1,12 +1,17 @@
-import { readServerFile } from "../../manager/storageManagement/reader/readServerFile.js";
 import { GetSetRemoveSessionStorageHelper } from "./clientStorage/GetSetRemoveSessionStorageHelper.js"
 
 class GetSetRemoveServerToClientHelper extends GetSetRemoveSessionStorageHelper{
     async getStorage(valueName, path) {
         let value;
-        value = await this.storage.getItem(valueName); 
+        value = await this.storage.getItem(valueName);
         if (value === null) {
-            value = await readServerFile(path);
+            try {
+                value = await fetch(path)
+                    .then(response => response.text())
+                    .catch(error => console.error('Error:', error));
+            } catch (error) { 
+
+            }
         } 
         return value;
     }
