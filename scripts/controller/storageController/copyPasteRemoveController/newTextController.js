fe2e27518e0d3af9895bd5d9cac334c69d846dc4
egnutv <101817247+egnutv@users.noreply.github.com>
2024-03-27 10:08:37 +0100

    Update newTextController.js

--- scripts/controller/storageController/copyPasteRemoveController/newTextController.js
+++ scripts/controller/storageController/copyPasteRemoveController/newTextController.js
@@ -1,10 +1,20 @@
-import { SiteController } from "./newSiteController";
+import { GetSetRemoveServerToClientHelper } from "../../../helper/storageHelper/GetSetRemoveServerToClientFileHelper.js";
+import { SiteController } from "./newSiteController.js";
 
 class TextController extends SiteController {
+    constructor() {
+        super();
+        this.stroage = new GetSetRemoveServerToClientHelper;
+        this.indexName = "indexOfSites";
+        this.indexPath = "data/packs/templates/sites/index.json";
+        this.pathToLang = "data/packs/texts/%lang%/";
+        this.standardLangFile = "config.json";
+        this.speceficLangFile = "specific/%file%.json";
+    }
     async getSet() {
         
     }
-    async get() {
+    async get(lang, indexEntry) {
     }
     async set() {
     }
