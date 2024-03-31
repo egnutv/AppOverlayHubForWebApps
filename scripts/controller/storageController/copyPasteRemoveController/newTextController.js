e84d9b3f136fc88dfa4fd9fa4148c1aefb84fcbe
egnutv <101817247+egnutv@users.noreply.github.com>
2024-03-31 22:39:44 +0200

    Update newTextController.js

--- scripts/controller/storageController/copyPasteRemoveController/newTextController.js
+++ scripts/controller/storageController/copyPasteRemoveController/newTextController.js
@@ -5,7 +5,7 @@ class TextController extends SiteController {
     constructor() {
         super();
         this.stroage = new GetSetRemoveServerToClientHelper;
-        this.indexName = "indexOfSites";
+        this.indexName = "indexOfTexts";
         this.indexPath = "data/packs/templates/sites/index.json";
         this.pathToLang = "data/packs/texts/%lang%/";
         this.standardLangFile = "config.json";
@@ -15,7 +15,17 @@ class TextController extends SiteController {
         //let arrayOfEntrys = await this.get(indexEntry);
     }
     async get(indexEntry) {
-        let valueOfEntry = await this.getEntryOf(this.indexName, this.indexPath); 
+        let valueOfEntry = await this.getEntryOf(this.indexName, this.indexPath);
+        console.log(valueOfEntry);
+        indexEntry = await this.convertToPath(indexEntry);
+        let value = valueOfEntry.index[indexEntry];
+
+        if (value != null || value != undefined || value != "" || value != null) {
+            value = indexEntry;
+        } else {
+            return console.error();
+        }
+
     }
     async set() {
 
