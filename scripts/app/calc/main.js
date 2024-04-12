f22d6c9a2acc19f6948cb4e171d9e8b81aa9ecbb
egnutv <101817247+egnutv@users.noreply.github.com>
2024-04-12 17:06:17 +0200

    Update main.js

--- scripts/app/calc/main.js
+++ scripts/app/calc/main.js
@@ -1,3 +1,24 @@
+import { selectDomElement } from "../../utils/selectDomElement"
+
 function testOut() {
     console.log("HALLO ICH BIN GELADEN")
+}
+
+class Calculator {
+    constructor() {
+
+    }
+    async copyToNumbar() {
+        let button = await selectDomElement(".click");
+        let destination = await selectDomElement(".result");
+
+        let b = button.value;
+        let d = destination.value;
+        let allowed = false;
+        if (b.length <= 1 || d.charAt(d.length - 1) != b) {
+            if(isNaN(d)){
+                destination.value += b;
+            }
+        }
+    }
 }
