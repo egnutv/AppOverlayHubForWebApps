9257d1cf1f158075f113681ff4f45fcfbea67de2
egnutv <101817247+egnutv@users.noreply.github.com>
2024-04-15 15:14:34 +0200

    Update: ScriptController

--- scripts/controller/storageController/copyPasteRemoveController/newScriptController.js
+++ scripts/controller/storageController/copyPasteRemoveController/newScriptController.js
@@ -4,11 +4,34 @@ class ScriptController extends SiteController {
     constructor() {
         super();
         this.indexPath = "data/configs/addScripts.json"
-        this.indexName = "ImportedScripts"
+        this.indexName = "ImportScript"
+        this.footName = "foot"; this.headName = "head";
     }
 
-    async get(indexENtry) {
-        let valueOfEntry;
+    async getSet(indexEntry) {
+        let arrayOfEntrys = await this.get(indexEntry);
+    }
+
+    async get(indexEntry) {
+        let valueOfEntry = await this.getEntryOf(this.indexName, this.indexPath);
+
+        let headEntry, footEntry;
+        try {
+            headEntry = valueOfEntry.add[indexEntry]?.head || null;
+            footEntry = valueOfEntry.add[indexEntry]?.foot || null;
+        } catch (error) {
+            console.error(error);
+        }
+
+        let value = "";
+        if (headEntry || footEntry) {
+            value += this.footName + ":" + footEntry + this.headName + ":" +  headEntry;
+        }
+        
+        return value;
+    }
+    async set() {
+
     }
 
 }
