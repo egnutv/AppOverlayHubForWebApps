0269c32c58beb610ec670d7ed925a6af82f80989
egnutv <101817247+egnutv@users.noreply.github.com>
2024-04-16 15:55:43 +0200

    Update ScriptControler

--- scripts/controller/storageController/copyPasteRemoveController/newScriptController.js
+++ scripts/controller/storageController/copyPasteRemoveController/newScriptController.js
@@ -1,3 +1,4 @@
+import { selectDomElement } from "../../../utils/selectDomElement.js";
 import { SiteController } from "./newSiteController.js";
 
 class ScriptController extends SiteController {
@@ -6,12 +7,26 @@ class ScriptController extends SiteController {
         this.indexPath = "data/configs/addScripts.json"
         this.indexName = "ImportScript"
         this.footName = "foot"; this.headName = "head";
+        this.laterScriptName = "|||[[[---LaterAddScript---]]]|||"
     }
     async getSet(indexEntry) {
+        /*try {
+            let elements = document.querySelectorAll(this.laterScriptName);
+            elements.forEach((element) => {
+                element.remove();
+            });
+        } catch (error) {
+            console.error(error);
+        }*/
+        await this.remove(this.laterScriptName);
+        console.log("BY PASSED")
+        
         let value = await this.get(indexEntry);
         console.log(value);
+        
         await this.set(value, indexEntry);
     }
+    
     async set(value, name) {
         console.log(value)
         let keys = Object.keys(value);
@@ -20,9 +35,48 @@ class ScriptController extends SiteController {
         for (let i = 0; i < numOfEntrys; i++) {
             let entry = keys[i]; // Ändern Sie numOfEntrys[i] in keys[i]
             console.log(entry);
-            let destination = entry;
+            let destination = await selectDomElement(entry);
+            console.log(destination)
+            /*let destiObj = document.createElement("script"); // Änderung hier
+            destiObj.className = this.laterScriptName;
+            destination.appendChild(destiObj); // Änderung hier
+            destination.appendChild(destiObj);*/
+
+            console.log(value[entry])
+
+            let finalKeys = Object.keys(value[entry])
+            let numOfFinalEntrys = finalKeys.length;
+            console.log(numOfFinalEntrys)
+
+            /*let objKeys = Object.keys(value[destination]);
+            let numOfObjKeys = objKeys.length;
+            console.log(numOfObjKeys);*/
+            
+            for (let j = 0; j < numOfFinalEntrys; j++) {
+                let finalDestination = finalKeys[j];
+                let finalKeyValue = value[entry][finalDestination]; // Abrufen des Werts, der dem Schlüssel zugeordnet ist
+                console.log("Key Value: " + finalKeyValue); // Ausgabe des zugeordneten Werts
+                let destiObj = document.createElement("script");
+                destiObj.className = this.laterScriptName;
+                destiObj.src = finalKeyValue;
+                destiObj.defer = true;
+                destination.appendChild(destiObj);
+            }
+            
         }
     }
+
+    async remove(className) {
+        let rmDestination = document.getElementsByClassName(className);
+        let rmDestinationSUM = rmDestination.length;
+            for (let i = 0; i < rmDestinationSUM; i++) {
+                let element = rmDestinationSUM[i];
+                element.remove();
+                
+            }
+        
+    }
+
     
 
     async get(indexEntry) {
@@ -38,11 +92,6 @@ class ScriptController extends SiteController {
         }
         return value;
     }
-    
-    
-    
-    
-
 }
 
 export { ScriptController }
