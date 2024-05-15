0ad4bd75a515a309cb1aaf6e155244bb7d848985
egnutv <101817247+egnutv@users.noreply.github.com>
2024-05-15 10:50:04 +0200

    add: A trafficLight GameStructor

---
+++ scripts/sites/apps/traffixLightGame/Entity.js
@@ -0,0 +1,8 @@
+class Entity {
+    constructor() {
+
+    }
+    async walk() {
+
+    }
+}
---
+++ scripts/sites/apps/traffixLightGame/TraffixLightGame.js
@@ -0,0 +1,8 @@
+class TraffixLightGame {
+    constructor() {
+        
+    }
+    async Light() {
+
+    }
+}
